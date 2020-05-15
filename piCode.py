af43a10318093ca49b095a093b6836c387e2ae33
Andres Colon <andresmcolon97@gmail.com>
2020-05-14 17:48:46 -0700

    Outputs BCD and AN Values, Updates Clock Each Minute

--- piCode.py
+++ piCode.py
@@ -5,22 +5,14 @@ from gpiozero import LEDBoard
 AN = LEDBoard(23, 24)
 cathode1 = LEDBoard(21, 20, 16, 12)
 cathode2 = LEDBoard(26, 19, 13, 6)
-# Obtain current time and set both minute and hour value
-# t = time.localtime()
-# currentHour = time.strftime("%H", t)
-# currentMin = time.strftime("%M", t)
-# 
-# # convert time into separate digits
-# leadHour = int(currentHour[0])
-# trailHour = int(currentHour[1])
-# leadMin = int(currentMin[0])
-# trailMin = int(currentMin[1])
+
+
 
 # needs to alternate between anode 0 and 1
 # initialize output to current time
 # increment each minute
 # reset after 12:59
-
+    
 # output BCD value for every value change    
 BCD = {
     0: (0, 0, 0, 0),
@@ -34,15 +26,31 @@ BCD = {
     8: (1, 0, 0, 0),
     9: (1, 0, 0, 1)
 }
-# cHour = 0
-# cMin = 0
+
+def getBCD(lHour, tHour, lMin, tMin):
+    AN.value = (1, 0)
+    cathode1.value = BCD.get(lHour)
+    cathode2.value = BCD.get(tHour)
+    time.sleep(.015)
+        
+    AN.value = (0, 1)
+    cathode1.value = BCD.get(lMin)
+    cathode2.value = BCD.get(tMin)
+    time.sleep(.015)
+
+tMin = time.time() + 60
 try:
     while True:
-        AN.value = (1, 0)
-        for i in range(10):
-            cathode2.value = BCD.get(i)
-            cathode1.value = BCD.get(i)
-            time.sleep(.5)
+        # Obtain current time and set both minute and hour value
+        currentTime = time.strftime("%H%M", time.localtime())
+        # separate time into individual digits
+        leadHour = int(currentTime[0])
+        trailHour = int(currentTime[1])
+        leadMin = int(currentTime[2])
+        trailMin = int(currentTime[3])
+        getBCD(leadHour, trailHour, leadMin, trailMin)
+        while time.time() < tMin:
+            getBCD(leadHour, trailHour, leadMin, trailMin)
 except KeyboardInterrupt:
     AN.value = (0, 0)
 
